18a9a0a3dca33193a13e13f8c007256acc77d69a
Laraib007 <lrbkhan01@gmail.com>
2024-09-10 20:16:01 +0500

    Fixed issues

--- redux/src/Navbar.js
+++ redux/src/Navbar.js
@@ -24,7 +24,6 @@ const Navbar = () => {
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
@@ -32,10 +31,7 @@ const Navbar = () => {
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+     
     </div>
   </div>
 </nav>

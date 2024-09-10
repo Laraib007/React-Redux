f820b40494e5c06d68ed2f58569dcd996dedab30
Laraib007 <lrbkhan01@gmail.com>
2024-09-10 21:29:07 +0500

    create withdrawal money func

--- redux/src/state/action-creator/index.js
+++ redux/src/state/action-creator/index.js
@@ -1,4 +1,13 @@
-export const deposotemoney =(amount)=>{
+export const depositemoney =(amount)=>{
+    return(dispatch)=>{
+        dispatch({
+            type: "deposite",
+            payload: amount
+        })
+    }
+}
+
+export const withdrawmoney =(amount)=>{
     return(dispatch)=>{
         dispatch({
             type: "deposite",

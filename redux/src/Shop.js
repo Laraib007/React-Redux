78401385e8f68878da077674ca3a02a2144b9880
Laraib007 <lrbkhan01@gmail.com>
2024-09-10 20:04:20 +0500

    adding a buttom compo

---
+++ redux/src/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <buttton>+</buttton>
+      Your Item Added
+      <buttton>-</buttton>
+    </div>
+  )
+}
+
+export default Shop

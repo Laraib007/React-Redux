45471ec499fab1ec9905b40361be4a409291f0f2
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-16 23:20:33 -0700

    creating seprate file for reducer

---
+++ redux/src/store/shopReducer.js
@@ -0,0 +1,15 @@
+const shopReducer = (state ={soap: 11}, action)=>{
+    if(action.type === "SELL_SOAP"){
+       return {
+           soap: state.soap - 1
+       }
+    }
+    if(action.type === "BUY_SOAP"){
+       return {
+           soap: state.soap + action.payload
+       }
+    }
+    return state
+   }
+
+   export default shopReducer;

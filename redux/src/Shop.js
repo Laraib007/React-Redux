50f3b4f57ea4b2f4fcc6b4136f24d8917056a13e
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-16 22:15:45 -0700

    fixing issues

--- redux/src/Shop.js
+++ redux/src/Shop.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { depositemoney } from './state/action-creator'
+import { useSelector } from 'react-redux'
 
 const Shop = () => {
+  const amount = useSelector(state => state)
+  const sellHandler =()=>{
+    depositemoney(1)
+  }
   return (
     <div>
         <h4>Your Shop</h4>
       <button type="button" class="mx-2 btn btn-primary">-</button>
-      Your Item Added
+      {amount}
       <button type="button" class=" mx-2 btn btn-primary">+</button>
     </div>
   )

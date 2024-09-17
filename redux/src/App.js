5c8bf18efbca43dc84495db58fca5337914de24c
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-16 23:24:22 -0700

    using component to replace string

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { BUY_SOAP, SELL_SOAP } from './store/shopActions';
 
 function App() {
   const soap = useSelector(state => state.soap)
   const dispatch = useDispatch()
   const handleSell =()=>{
-    dispatch({type: "SELL_SOAP"})
+    dispatch({type: SELL_SOAP})
   }
   const handleBuy =()=>{
-    dispatch({type: "BUY_SOAP", payload: 10})
+    dispatch({type: BUY_SOAP, payload: 10})
   }
   return (
     <div className="App">

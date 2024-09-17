3c88de23ea1f3de5ed8fbc05a92d1aba5b32f7a6
Laraib007 <125761184+Laraib007@users.noreply.github.com>
2024-09-16 23:22:27 -0700

    seprating actions

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,7 +1,5 @@
 import {createStore} from "redux"
 import shopReducer from "./shopReducer";
 
-
-
 const store = createStore(shopReducer)
 export default store;
--- redux/src/store/shopReducer.js
+++ redux/src/store/shopReducer.js
@@ -1,10 +1,12 @@
+import { BUY_SOAP, SELL_SOAP } from "./shopActions";
+
 const shopReducer = (state ={soap: 11}, action)=>{
-    if(action.type === "SELL_SOAP"){
+    if(action.type === SELL_SOAP){
        return {
            soap: state.soap - 1
        }
     }
-    if(action.type === "BUY_SOAP"){
+    if(action.type === BUY_SOAP){
        return {
            soap: state.soap + action.payload
        }
